4b45dacfff79618c5bccef118330a64e7646f81d
costel-dev <costelcuconoiu@gmail.com>
2020-03-26 11:12:40 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let billAmount = document.getElementById("bill-amount");
+let percentage = document.getElementById("percentage");
+
+function calculate() {
+  let tipAmount = parseFloat(billAmount.value * (percentage.value / 100));
+  let total = parseFloat(billAmount.value) + tipAmount;
+  console.log(total);
+  document.getElementById("tip-amount").value = "€ " + tipAmount;
+  document.getElementById("total").value = "€ " + total;
+}
