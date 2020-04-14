a87fd0738f8c6b0cfaf7bb25cd73895860401a57
costel-dev <costelcuconoiu@gmail.com>
2020-04-14 10:48:57 +0200

    updating the title

--- js/script.js
+++ js/script.js
@@ -3,8 +3,9 @@ let percentage = document.getElementById("percentage");
 
 function calculate() {
   let tipAmount = parseFloat(billAmount.value * (percentage.value / 100));
+  // console.log(tipAmount);
   let total = parseFloat(billAmount.value) + tipAmount;
-  console.log(total);
+  // console.log(total);
   document.getElementById("tip-amount").value = "€ " + tipAmount;
   document.getElementById("total").value = "€ " + total;
 }
